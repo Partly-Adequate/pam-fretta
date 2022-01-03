da97f8617f5732e3634d1c92f2520d21dc8c91e4
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-03 17:39:37 +0100

    Initial Code

---
+++ pam_fretta/lua/pam/client/extensions/fretta_votescreen.lua
@@ -0,0 +1,42 @@
+PAM_EXTENSION.name = "fretta_votescreen"
+PAM_EXTENSION.enabled = false
+local panel = nil
+
+function PAM_EXTENSION:OnVoteStarted()
+	panel = vgui.Create("pam_fretta_votescreen")
+end
+
+function PAM_EXTENSION:OnVoteCanceled()
+	panel:Remove()
+end
+
+function PAM_EXTENSION:OnVoterAdded(ply, map_id)
+	panel:AddVoter(ply)
+end
+
+function PAM_EXTENSION:OnWinnerAnnounced()
+	panel:Flash(PAM.winning_option_id)
+end
+
+function PAM_EXTENSION:ToggleVisibility()
+	panel:SetVisible(not panel:IsVisible())
+end
+
+function PAM_EXTENSION:OnEnable()
+	if PAM.state != PAM.STATE_DISABLED then
+		self:OnVoteStarted()
+		for steam_id, option_id in pairs(PAM.votes) do
+			self:OnVoterAdded(player.GetBySteamID(steam_id), option_id)
+		end
+	end
+	if PAM.state == PAM.STATE_FINISHED then
+		self:OnWinnerAnnounced()
+	end
+end
+
+function PAM_EXTENSION:OnDisable()
+	if PAM.state != PAM.STATE_DISABLED then
+		timer.Remove("PAM_FrettaBlip")
+		panel:Remove()
+	end
+end
